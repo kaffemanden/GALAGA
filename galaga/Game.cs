18ae924d60fcdb136183e22296ef43055665fd5b
kaffemanden <55875069+kaffemanden@users.noreply.github.com>
2020-02-26 14:31:52 +0100

    Update Game.cs

--- galaga/Game.cs
+++ galaga/Game.cs
@@ -7,55 +7,49 @@ using DIKUArcade.Graphics;
 using System;
 using System.IO;
 
-namespace galaga
-{
+
 public class Game : IGameEventProcessor<object> {
     private Window win;
     private DIKUArcade.Timers.GameTimer gameTimer;
 
-    private Player player = new Player(
-    new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
-    new Image(Path.Combine("Assets", "Images", "Player.png")));
-
+    private Player player;
     public Game() {
 // TODO: Choose some reasonable values for the window and timer constructor. // For the window, we recommend a 500x500 resolution (a 1:1 aspect ratio). 
-    
     win = new Window("Galaga", 500, 500);
     gameTimer = new GameTimer(60,60);
-      }
+    player = new Player(
+        new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+        new Image(Path.Combine("Assets", "Images", "Player.png")));
+    }
     public void GameLoop() {
-
-
         while(win.IsRunning()) { 
             gameTimer.MeasureTime();
             while (gameTimer.ShouldUpdate()) {
                 win.PollEvents();
             // Update game logic here
-        }
+            }
             if (gameTimer.ShouldRender()) { 
                 win.Clear();
+                player.Entity.RenderEntity();
             // Render gameplay entities here
                 win.SwapBuffers(); 
-                player.Entity.RenderEntity();
-        }
+            }
 
-        if (gameTimer.ShouldReset()) {
-    // 1 second has passed - display last captured ups and fps 
-            win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
-            ", FPS: " + gameTimer.CapturedFrames;
+            if (gameTimer.ShouldReset()) {
+                // 1 second has passed - display last captured ups and fps 
+                win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
+                            ", FPS: " + gameTimer.CapturedFrames;
             }
         }
-}
+    }
     
     public void KeyPress(string key) {
         throw new NotImplementedException();
-}
+    }
     public void KeyRelease(string key) {
         throw new NotImplementedException();
-}
-    public void ProcessEvent(GameEventType eventType,
-        GameEvent<object> gameEvent) {
-        throw new NotImplementedException();
-        } 
     }
+    public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
+        throw new NotImplementedException();
+    } 
 }
